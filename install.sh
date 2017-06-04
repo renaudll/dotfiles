248eb0a8533ba6e4daf0ea28b045904851548e78
Renaud Lessard Larouche <sigmao@gmail.com>
2017-06-04 19:20:08 -0400

    remove fish

--- install.sh
+++ install.sh
@@ -6,9 +6,6 @@ ln -s ~/dotfiles/bash/bashrc ~/.bashrc
 # Configure zsh
 ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
 
-# Configure fish
-ln -s ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
-
 # Configure git
 ln -s ~/dotfiles/git/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/git/gitignore ~/.gitignore
