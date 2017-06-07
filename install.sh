60cedb999e28b3530be8d28e69638bffd25a6df7
Renaud Lessard Larouche <sigmao@gmail.com>
2017-06-07 03:54:11 +0000

    update .bashrc, disable .zshrc

--- install.sh
+++ install.sh
@@ -4,32 +4,34 @@
 ln -s ~/dotfiles/bash/bashrc ~/.bashrc
 
 # Configure zsh
-ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
+# ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
 
 # Configure git
 ln -s ~/dotfiles/git/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/git/gitignore ~/.gitignore
 
 # Configure tmux
-ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+# ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 
 # Configure vim
-ln -s ~/dotfiles/vim ~/.vim
-ln -s ~/dotfiles/vimrc ~/.vimrc
+# ln -s ~/dotfiles/vim ~/.vim
+# ln -s ~/dotfiles/vimrc ~/.vimrc
 
-# Configure .config/autostart
-ln -s ~/dotfiles/config_autostart ~/.config/autostart
+# Configure .config (autostart, powerline)
+mkdir -p ~/.config
+ln -s ~/dotfiles/config/autostart ~/.config/autostart
+ln -s ~/dotfiles/config/powerline ~/.config/powerline
 
 # Update git submodules
-cd ~/dotfiles
-git submodule update --init
+# cd ~/dotfiles
+# git submodule update --init
 
-cd ~/dotfiles/zsh/zsh-autosuggestions
-git submodule update --init
+# cd ~/dotfiles/zsh/zsh-autosuggestions
+# git submodule update --init
 
-cd ~/dotfiles/vim/bundle/YouCompleteMe
-git submodule update --init --recursive
+# cd ~/dotfiles/vim/bundle/YouCompleteMe
+# git submodule update --init --recursive
 
 # Compile ycmd for usage with python (once)
-cd ~/dotfiles/vim/bundle/YouCompleteMe
-./install.py
+# cd ~/dotfiles/vim/bundle/YouCompleteMe
+# ./install.py
